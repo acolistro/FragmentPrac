87095f5f24b81e1bc6399438a977fd7cf36f0806
arcolistro <arcolistro@gmail.com>
2020-07-15 06:33:18 -0700

    add more layouts and create data class

--- app/src/main/java/com/example/fragmentprac/MainFragment.java
+++ app/src/main/java/com/example/fragmentprac/MainFragment.java
@@ -20,7 +20,7 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.my_fragment, container, false);
 
         return view;
     }
---
+++ app/src/main/java/com/example/fragmentprac/data/Course.java
@@ -0,0 +1,28 @@
+package com.example.fragmentprac.data;
+
+import android.content.Context;
+
+public class Course {
+    private String courseName;
+    private String courseImage;
+
+    public int getImageResourceId(Context context){
+        return context.getResources().getIdentifier(this.courseName, "drawable", context.getPackageName());
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseImage() {
+        return courseImage;
+    }
+
+    public void setCourseImage(String courseImage) {
+        this.courseImage = courseImage;
+    }
+}
